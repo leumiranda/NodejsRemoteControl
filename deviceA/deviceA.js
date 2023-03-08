59dac02a9460ae19317fc5a2ba9f8ba4b71b83d2
Mutneja <118028587+leumiranda@users.noreply.github.com>
2023-03-08 07:00:25 -0300

    Add files via upload

--- deviceA/deviceA.js
+++ deviceA/deviceA.js
@@ -1,27 +1,37 @@
 const mqtt = require('mqtt');
 const readline = require('readline');
-const client = mqtt.connect('mqtt://test.mosquitto.org');
+const dns = require('dns');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-client.on('connect', function () {
-  console.log('Conectado ao broker MQTT');
+const brokerUrl = 'mqtt://test.mosquitto.org';
+const brokerHostname = brokerUrl.substring(brokerUrl.indexOf('//') + 2);
 
-  rl.on('line', function (input) {
-    if (input === 'reiniciar') {
-      client.publish('suporte', 'reiniciar');
-    }
-    if (input === 'notepad') {
-      client.publish('suporte', 'notepad');
-    }
-    if (input === 'calc') {
-      client.publish('suporte', 'calc');
-    }
-    if (input === 'browser') {
-      client.publish('suporte', 'browser');
-    }
-  });
+dns.lookup(brokerHostname, function (err, address) {
+  if (err || !address) {
+    console.log('Não foi possível resolver o endereço do broker MQTT.');
+    process.exit(1);
+  } else {
+    const client = mqtt.connect(brokerUrl);
+    client.on('connect', function () {
+      console.log('Conectado ao broker MQTT');
+      rl.on('line', function (input) {
+        if (input === 'reiniciar') {
+          client.publish('suporte', 'reiniciar');
+        }
+        if (input === 'notepad') {
+          client.publish('suporte', 'notepad');
+        }
+        if (input === 'calc') {
+          client.publish('suporte', 'calc');
+        }
+        if (input === 'browser') {
+          client.publish('suporte', 'browser');
+        }
+      });
+    });
+  }
 });
